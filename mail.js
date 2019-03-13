1e886629d400fe768dc0e1167f9a5a41cb167c55
Briton Westerhaus <Briton.Westerhaus@gmail.com>
2019-03-12 22:41:26 -0400

    Form processing
    Added some form processing to test.

--- mail.js
+++ mail.js
@@ -1,4 +1,6 @@
+const http = require('http');
 const nodemailer = require('nodemailer');
+const port = 8080;
 
 let transporter = nodemailer.createTransport({
     sendmail: true,
@@ -13,4 +15,31 @@ transporter.sendMail({
 }, (err, info) => {
     console.log(info.envelope);
     console.log(info.messageId);
+});
+
+http.createServer((req, res) => {
+	if (req.method === 'POST') {
+        let message = '';
+        let subject = '';
+		req.on('data', chunk => {
+            chunk = chunk.toString().split('=');
+            if (chunk[0] == "name")
+                subject = "Message from " + chunk[1];
+            if (chunk[0] == "message")
+                message = chunk[1]; 
+		});
+		req.on('end', () => {
+            transporter.sendMail({
+                from: 'me@BritonWesterhaus.com',
+                to: 'Briton.Westerhaus@gmail.com',
+                subject: subject,
+                text: message
+            }, (err, info) => {
+                console.log(info.envelope);
+                console.log(info.messageId);
+            });
+		});
+	}
+}).listen(port, () => {
+	console.log('listening on: http://localhost:%s', port);
 });
--- test.js
+++ test.js
@@ -5,26 +5,37 @@ const port = 8080;
 const emitter = new events.EventEmitter();
 let name = "";
 
-emitter.on('enter', function(name, socket) {
-	console.log("Hello, " + name + "!");
-	socket.write("Hello, " + name + "!");
+//emitter.on('enter', function(name, socket) {
+//	console.log("Hello, " + name + "!");
+//	socket.write("Hello, " + name + "!");
+//});
+
+//net.createServer(socket => {
+//	if (name == "")
+//		socket.write("What is your name?")
+//	socket.on('data', data => {
+//		dataStr = data.toString();
+//		if (dataStr == "\r" || dataStr == "\n" || dataStr == "\r\n") {
+//			emitter.emit('enter', name, socket);
+//			socket.destroy();
+//		} else
+//			name += dataStr;
+//	})
+//}).listen(port);
+
+
+
+http.createServer((req, res) => {
+	if (req.method === 'POST') {
+		let body = '';
+		req.on('data', chunk => {
+			body += chunk.toString(); // convert Buffer to string
+		});
+		req.on('end', () => {
+			console.log(body);
+			res.end('ok');
+		});
+	}
+}).listen(port, () => {
+	console.log('listening on: http://localhost:%s', port);
 });
-
-net.createServer(socket => {
-	if (name == "")
-		socket.write("What is your name?")
-	socket.on('data', data => {
-		dataStr = data.toString();
-		if (dataStr == "\r" || dataStr == "\n" || dataStr == "\r\n") {
-			emitter.emit('enter', name, socket);
-			socket.destroy();
-		} else
-			name += dataStr;
-	})
-}).listen(port);
-
-// http.createServer((req, res) => {
-// 	res.end('Testing chained callbacks - editing through scp.');
-// }).listen(port, () => {
-// 	console.log('listening on: http://localhost:%s', port);
-// });
